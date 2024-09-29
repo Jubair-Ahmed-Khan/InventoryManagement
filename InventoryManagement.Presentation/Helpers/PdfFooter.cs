21437ec25f92452b9aba2ccf1d09a0d84395ea72
Jubair-Ahmed-Khan <jubairahmedkhantonmoy@gmail.com>
2024-09-29 22:39:04 +0600

    Product Report Added

---
+++ InventoryManagement.Presentation/Helpers/PdfFooter.cs
@@ -0,0 +1,30 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text;
+
+namespace InventoryManagement.Presentation.Helpers;
+
+public class PdfFooter : PdfPageEventHelper
+{
+    public override void OnEndPage(PdfWriter writer, Document document)
+    {
+        Font footerFont = FontFactory.GetFont("Arial", 10, Font.ITALIC);
+
+        PdfPTable footerTable = new PdfPTable(1)
+        {
+            TotalWidth = 300,
+            HorizontalAlignment = Element.ALIGN_CENTER
+        };
+
+        PdfPCell cell = new PdfPCell(new Phrase("Page " + writer.PageNumber, footerFont))
+        {
+            Border = Rectangle.NO_BORDER,
+            HorizontalAlignment = Element.ALIGN_CENTER
+        };
+
+        footerTable.AddCell(cell);
+
+        float xPosition = (document.PageSize.Width - footerTable.TotalWidth) / 2;
+
+        footerTable.WriteSelectedRows(0, -1, xPosition, 30, writer.DirectContent);
+    }
+}
